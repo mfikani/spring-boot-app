5aa2af56e107020de29f29ac32524f98401601ce
mfi <miled.fikani@gmail.com>
2017-05-19 16:20:54 +0300

    first commit, fresh start

---
+++ src/main/java/com/propertyunderconstruction/FirstController.java
@@ -0,0 +1,14 @@
+package com.propertyunderconstruction;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+	
+	@RequestMapping("/hello")
+	public String hello(){
+		return "Hello Under Contruction";
+	}
+
+}
