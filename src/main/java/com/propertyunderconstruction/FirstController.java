118748102d4f84c451df09d02af286014d2286be
Toufic TALIB <toufictalib@gmail.com>
2017-05-24 09:56:17 +0300

    Add CLient Project

--- src/main/java/com/propertyunderconstruction/FirstController.java
+++ src/main/java/com/propertyunderconstruction/FirstController.java
@@ -1,14 +1,28 @@
 package com.propertyunderconstruction;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.propertyunderconstruction.user.model.Client;
+import com.propertyunderconstruction.user.service.ClientInterface;
+
 @RestController
 public class FirstController {
-	
+
+	@Autowired
+	private ClientInterface ClientInterface;
+
 	@RequestMapping("/hello")
-	public String hello(){
-		return "Hello Under Contruction";
+	public String hello() {
+		List<Client> clients = ClientInterface.getClients();
+		StringBuilder builder = new StringBuilder();
+		for (Client client : clients) {
+			builder.append(client.toString());
+		}
+		return builder.toString();
 	}
 
 }
--- src/main/java/com/propertyunderconstruction/SpringBootAppApplication.java
+++ src/main/java/com/propertyunderconstruction/SpringBootAppApplication.java
@@ -2,8 +2,10 @@ package com.propertyunderconstruction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:starter.xml")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
